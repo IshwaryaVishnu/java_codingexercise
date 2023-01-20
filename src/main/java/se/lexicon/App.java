b9f1a59183188a5bc0682e481b0d4b98932ec844
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-01-20 16:35:47 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,16 @@
+package se.lexicon;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String str = "Java code";
+        StringBuilder str2 = new StringBuilder();
+        str2.append(str);
+        str2 = str2.reverse();     // used string builder to reverse
+        System.out.println(str2);
+    }
+
+}
+
